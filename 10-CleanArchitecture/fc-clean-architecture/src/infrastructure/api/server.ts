1faf19444898e68ffafd0d4ee010a98d7355db2a
Eric Quadros <ericquadros7@gmail.com>
2025-02-22 19:27:59 -0300

    Cleaning the solution

--- 10-CleanArchitecture/fc-clean-architecture/src/infrastructure/api/server.ts
+++ 10-CleanArchitecture/fc-clean-architecture/src/infrastructure/api/server.ts
@@ -6,20 +6,6 @@ import sequelize from "../../database/sequelize";
 dotenv.config();
 const port: number = Number(process.env.PORT) || 3000;
 
-// Usando as rotas de produtos
-// app.use("/", productRoute); // Isso significa que as rotas de produtos estarão acessíveis em /api/products
-
-// // Sincronizando o banco de dados
-// sequelize.sync()
-//   .then(() => {
-//     app.listen(port, () => {
-//       console.log(`Server is listening on port ${port}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error("Unable to connect to the database:", error);
-//   });
-
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 }); 

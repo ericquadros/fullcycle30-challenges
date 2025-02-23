01b62a8956e23c12347d348e937713fffc0bafd6
Eric Quadros <ericquadros7@gmail.com>
2025-02-22 22:23:06 -0300

    Tests passing

--- 11-SistemasMonoliticos/fc-monolito/src/modules/client-adm/usecase/add-client/add-client.usecase.ts
+++ 11-SistemasMonoliticos/fc-monolito/src/modules/client-adm/usecase/add-client/add-client.usecase.ts
@@ -1,8 +1,8 @@
-import Address from "../../../@shared/domain/value-object/address";
 import Id from "../../../@shared/domain/value-object/id.value-object";
+import Address from "../../../@shared/domain/value-object/address";
 import Client from "../../domain/client.entity";
 import ClientGateway from "../../gateway/client.gateway";
-import { AddClientInputDto, AddClientOutputDto } from "./add-client.usecase.dto";
+import { AddClientInputDto, AddClientOutputDto } from "./add-client.dto";
 
 export default class AddClientUseCase {
 
@@ -15,18 +15,18 @@ export default class AddClientUseCase {
   async execute(input: AddClientInputDto): Promise<AddClientOutputDto> {
 
     const props = {
-      id: new Id(input.id) || new Id(),
+      id: new Id(),
       name: input.name,
       email: input.email,
       document: input.document,
       address: new Address(
-        input.address.street,
-        input.address.number,
-        input.address.complement,
-        input.address.city,
-        input.address.state,
-        input.address.zipCode,
-      )
+        input.street,
+        input.number,
+        input.complement,
+        input.city,
+        input.state,
+        input.zipCode
+      ),
     }
 
     const client = new Client(props)
@@ -37,14 +37,12 @@ export default class AddClientUseCase {
       name: client.name,
       email: client.email,
       document: client.document,
-      address: new Address(
-        client.address.street,
-        client.address.number,
-        client.address.complement,
-        client.address.city,
-        client.address.state,
-        client.address.zipCode,
-      ),
+      street: client.address.street,
+      number: client.address.number,
+      complement: client.address.complement,
+      city: client.address.city,
+      state: client.address.state,
+      zipCode: client.address.zipCode,
       createdAt: client.createdAt,
       updatedAt: client.updatedAt
     }
--- 11-SistemasMonoliticos/fc-monolito/src/server.ts
+++ 11-SistemasMonoliticos/fc-monolito/src/server.ts
@@ -1,18 +1,38 @@
-import { setupApp } from "./api/express";
+import { app } from "./api/express";
+import { Sequelize } from "sequelize-typescript";
+import { ProductModel } from "./modules/product-adm/repository/product.model";
+import { ClientModel } from "./modules/client-adm/repository/client.model";
+import { InvoiceModel } from "./modules/invoice/repository/invoice.model";
+import { OrderModel } from "./modules/checkout/repository/order.model";
+import OrderItemModel from "./modules/checkout/repository/order-item.model";
+import InvoiceItemModel from "./modules/invoice/repository/invoice-item.model";
 
 const port = 3000;
 
-async function startServer() {
-  try {
-    const app = await setupApp();
-    
-    app.listen(port, () => {
-      console.log(`Server rodando na porta ${port}`);
-    });
-  } catch (error) {
-    console.error("Unable to start the server:", error);
-    process.exit(1);
-  }
+async function bootstrap() {
+  const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: ":memory:",
+    logging: false,
+    sync: { force: true },
+  });
+
+  sequelize.addModels([
+    ProductModel,
+    ClientModel,
+    InvoiceModel,
+    InvoiceItemModel,
+    OrderModel,
+    OrderItemModel,
+  ]);
+  await sequelize.sync();
+
+  app.listen(port, () => {
+    console.log(`Server rodando na porta ${port}`);
+  });
 }
 
-startServer();
+bootstrap().catch((error) => {
+  console.error("Failed to start server:", error);
+  process.exit(1);
+});
